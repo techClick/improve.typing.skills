d80a424e0ad7423699af0814b7a5bbcd921fd1d0
Ikechi Anya <ikechi@agures.com>
2022-05-19 13:40:24 +0100

    Deployment changes 5

--- views/Challenge/Timer/Timer.tsx
+++ views/Challenge/Timer/Timer.tsx
@@ -2,19 +2,19 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../../redux/hooks';
 import { selectHasSynced } from '../../../redux/store';
-import { cancelTimeout, selectMinsLeft, selectSecsLeft, setMinsLeft, setSecsLeft, setShowPopup, setTimer } from '../redux';
+import { cancelTimeout, selectHasCompletedChallenge, selectMinsLeft, selectSecsLeft, setMinsLeft, setSecsLeft, setShowPopup, setTimer } from '../redux';
 import ScoreBoard from '../ScoreBoard/ScoreBoard';
 import * as S from './Timer.styled';
 
 const Timer = () => {
   const hasSyncedWithStorage = useAppSelector(selectHasSynced);
+  const hasCompletedChallenge = useAppSelector(selectHasCompletedChallenge);
   const minsLeft = useAppSelector(selectMinsLeft);
   const secsLeft = useAppSelector(selectSecsLeft);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (hasSyncedWithStorage && (minsLeft > 0 || secsLeft > 0)) {
-      console.log(secsLeft, minsLeft)
+    if (hasSyncedWithStorage && !hasCompletedChallenge) {
       dispatch(setTimer(setTimeout(() => {
         if (secsLeft > 0) {
           dispatch(setSecsLeft(secsLeft - 1));
